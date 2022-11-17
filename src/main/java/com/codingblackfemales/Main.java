38858c6479cfcc85eafe3bcbbf64cf43f94394bb
Solange Umuhire Gasengayire <sgasengayire@gmail.com>
2022-11-17 19:39:22 +0000

    Try to display composition in code

--- src/main/java/com/codingblackfemales/Main.java
+++ src/main/java/com/codingblackfemales/Main.java
@@ -17,15 +17,16 @@ public class Main {
         final StrawberryCake strawberryCake = new StrawberryCake();
 
         // Create decorated cakes
-        final SprinkledCake sprinkledCake = new SprinkledCake(vanillaCake);
         final WithSayingCake plainVanillaCake = new WithSayingCake(vanillaCake, "\"PLAIN!\"");
-        final WithSayingCake fancySprinkledVanillaCake = new WithSayingCake(sprinkledCake, "\"FANCY!\"");
-
-        final SprinkledCake sprinkledStrawberryCake = new SprinkledCake(strawberryCake);
-        final SprinkledCake doubleSprinkledStrawberryCake = new SprinkledCake(sprinkledStrawberryCake);
-        final WithSayingCake oneOfCake = new WithSayingCake(doubleSprinkledStrawberryCake, "\"One of\"");
-        final WithSayingCake oneOfWithEverything = new WithSayingCake(oneOfCake, "\"EVERYTHING\"");
-        final MultiLayeredCake multiLayeredCake = new MultiLayeredCake(oneOfWithEverything);
+        final WithSayingCake fancySprinkledVanillaCake = new WithSayingCake(new SprinkledCake(vanillaCake), "\"FANCY!\"");
+        final MultiLayeredCake multiLayeredCake = new MultiLayeredCake(
+                new WithSayingCake(
+                        new WithSayingCake(
+                                new SprinkledCake(
+                                        new SprinkledCake(strawberryCake)),
+                                "\"One of\""),
+                        "\"EVERYTHING\"")
+        );
 
         order.addCake(chocolateCake);
         order.addCake(plainVanillaCake);
